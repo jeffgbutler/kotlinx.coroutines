e90324ff890ef329c1e3e287e3bb1b18053185f0
Jeff Butler <jeffgbutler@gmail.com>
2024-12-04 17:20:55 -0500

    Develocity Setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,15 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.develocity") version "3.18.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
+}
+
+develocity {
+    server = "https://develocity.jgb-lab.dev"
+}
+
 rootProject.name = "kotlinx.coroutines"
 
 fun module(path: String) {
